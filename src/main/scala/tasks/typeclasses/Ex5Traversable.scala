46a5c7c073248745bb2bad434c3c4b411fe05c09
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-03-25 12:14:52 +0100

    Task 5 - 'Traversable'

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,8 @@
 package u04lab
 import u03.Sequences.* 
 import Sequence.*
+import u03.Optionals.*
+import Optional.*
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -19,8 +21,29 @@ object Ex5Traversable:
 
   def log[A](a: A): Unit = println("The next element is: "+a)
 
-  def logAll[A](seq: Sequence[A]): Unit = seq match
-    case Cons(h, t) => log(h); logAll(t)
-    case _ => ()
+  def logAll[T[_]: Traversable, A](t: T[A]): Unit =
+    val traversable = summon[Traversable[T]]
+    traversable.consumer(t)(log)
 
-  
+  trait Traversable[T[_]]:
+    def consumer[A](t: T[A])(c: A => Unit): Unit
+
+  given Traversable[Optional] with
+    def consumer[A](t: Optional[A])(c: A => Unit): Unit = t match
+      case Just(a) => c(a)
+      case _ => ()
+
+  given Traversable[Sequence] with
+    def consumer[A](t: Sequence[A])(c: A => Unit): Unit = t match
+      case Cons(h, t) => c(h); consumer(t)(c)
+      case _ => ()
+
+@main def tryGeneralisation =
+  import Ex5Traversable.{*, given}
+  val value1: Optional[Int] = Just(7)
+  val cons1: Int => Unit = a => print(a)
+  println:
+    logAll(value1)
+  val list1 = Cons(10, Cons(20, Nil()))
+  println:
+    logAll(list1)
